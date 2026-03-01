bec320d476eb72b2173da972cdb45219ce59cafb
WHITEROSE <firasmoussa60@gmail.com>
2026-03-01 01:28:25 +0100

    postgresql setup

--- src/main/java/com/org/fleetmanagement/vehicle/Car.java
+++ src/main/java/com/org/fleetmanagement/vehicle/Car.java
@@ -1,7 +1,12 @@
 package com.org.fleetmanagement.vehicle;
 
+import jakarta.persistence.Entity;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Entity
+@NoArgsConstructor
 public class Car extends Vehicle {
     public Car(UUID id, String make, String model, int year) {
         super(id, make, model, year);
--- src/main/java/com/org/fleetmanagement/vehicle/Vehicle.java
+++ src/main/java/com/org/fleetmanagement/vehicle/Vehicle.java
@@ -1,19 +1,30 @@
 package com.org.fleetmanagement.vehicle;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
 public abstract class Vehicle {
-    private final UUID id;
-    private final String make;
-    private final String model;
-    private final int year;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String make;
+    private String model;
+    private int year;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     public Vehicle(UUID id, String make, String model, int year) {
